09f7d154cb78c1ffdb046fc3f6626f22bf1a7e87
webtrainer-in <webtrainer-in@gmail.com>
2019-12-22 14:04:17 +0530

    Add component to add new property and implement Routing
    Signed-off-by: webtrainer-in <webtrainer-in@gmail.com>

--- Housing-SPA/src/app/app.module.ts
+++ Housing-SPA/src/app/app.module.ts
@@ -2,22 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BindingComponent } from './binding/binding.component';
 import { PropertyCardComponent } from './property-card/property-card.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { HousingService } from './Services/housing.service';
+import { AddPropertyComponent } from './add-property/add-property.component';
+
+const appRoutes: Routes = [
+  { path:'', component: PropertyListComponent    },
+  { path:'add-property', component: AddPropertyComponent }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     BindingComponent,
     PropertyCardComponent,
-    PropertyListComponent
+    PropertyListComponent,
+    AddPropertyComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
